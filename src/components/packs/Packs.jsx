c1d83b6329435200e95e6c91665a7859cf108309
Ridwan <129216149+Ridwan-A-wae@users.noreply.github.com>
2023-09-07 18:31:24 +0700

    update

--- src/components/packs/Packs.jsx
+++ src/components/packs/Packs.jsx
@@ -46,7 +46,7 @@ function Packs() {
             alt=""
           />
           <div className="midItemCard">
-            <h2 className="itemNameShop">Packs Canvas Pack</h2>
+            <h2 className="itemNameShop">Pack 1</h2>
             <p className="itemPriceShop">$ 50.00 USD</p>
             <div className="btnConItemShop">
               <button className="btnItemShop">Detail</button>
@@ -57,11 +57,11 @@ function Packs() {
         <div className="itemCardShop">
           <img
             className="imgItemPicCardShop"
-            src="https://images.unsplash.com/photo-1520963959303-a5cc3bdf9260?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2130&q=80"
+            src="https://images.unsplash.com/photo-1622260614153-03223fb72052?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2187&q=80"
             alt=""
           />
           <div className="midItemCard">
-            <h2 className="itemNameShop">coffee camping kit</h2>
+            <h2 className="itemNameShop">Pack 2</h2>
             <p className="itemPriceShop">$ 80.00 USD</p>
             <div className="btnConItemShop">
               <button className="btnItemShop">Detail</button>
@@ -72,11 +72,11 @@ function Packs() {
         <div className="itemCardShop">
           <img
             className="imgItemPicCardShop"
-            src="https://images.unsplash.com/photo-1628087235616-4e146afcd061?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2188&q=80"
+            src="https://images.unsplash.com/photo-1622260614153-03223fb72052?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2187&q=80"
             alt=""
           />
           <div className="midItemCard">
-            <h2 className="itemNameShop">White tent</h2>
+            <h2 className="itemNameShop">Pack 3 </h2>
             <p className="itemPriceShop">$ 200.00 USD</p>
             <div className="btnConItemShop">
               <button className="btnItemShop">Detail</button>
--- src/components/tents/Tents.jsx
+++ src/components/tents/Tents.jsx
@@ -41,11 +41,11 @@ function tents() {
           <div className="itemCardShop">
             <img
               className="imgItemPicCardShop"
-              src="https://images.unsplash.com/photo-1622260614153-03223fb72052?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2187&q=80"
+              src="https://images.unsplash.com/photo-1628087235616-4e146afcd061?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2188&q=80"
               alt=""
             />
             <div className="midItemCard">
-              <h2 className="itemNameShop">Tents Canvas Pack</h2>
+              <h2 className="itemNameShop">Tents 1 </h2>
               <p className="itemPriceShop">$ 50.00 USD</p>
               <div className="btnConItemShop">
                 <button className="btnItemShop">Detail</button>
@@ -56,11 +56,11 @@ function tents() {
           <div className="itemCardShop">
             <img
               className="imgItemPicCardShop"
-              src="https://images.unsplash.com/photo-1520963959303-a5cc3bdf9260?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2130&q=80"
+              src="https://images.unsplash.com/photo-1628087235616-4e146afcd061?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2188&q=80"
               alt=""
             />
             <div className="midItemCard">
-              <h2 className="itemNameShop">coffee camping kit</h2>
+              <h2 className="itemNameShop">Tents 2</h2>
               <p className="itemPriceShop">$ 80.00 USD</p>
               <div className="btnConItemShop">
                 <button className="btnItemShop">Detail</button>
@@ -75,7 +75,7 @@ function tents() {
               alt=""
             />
             <div className="midItemCard">
-              <h2 className="itemNameShop">White tent</h2>
+              <h2 className="itemNameShop">Tents 3</h2>
               <p className="itemPriceShop">$ 200.00 USD</p>
               <div className="btnConItemShop">
                 <button className="btnItemShop">Detail</button>
