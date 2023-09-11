1a1d3c3831e090813f1b773cfe2592b66844edc8
Ridwan <129216149+Ridwan-A-wae@users.noreply.github.com>
2023-09-11 17:57:09 +0700

    Update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <div className="conContact">
+        <div className="Serving">
+          <h1 className="headF">Contact Acme Outdoors</h1>
+          <div className="cardsContact">
+            <div className="cardContact">
+              <div className="contactLogo">📞</div>
+              <div className="headContact">Contact Us</div>
+              <div className="footContact">
+                Just want to say hi? We'd love to hear from you. We love our
+                customers and community!{" "}
+              </div>
+              <div className="btnWrapperContact">
+                <button className="btnContact">Send Us A Message</button>
+              </div>
+            </div>
+            <div className="cardContact">
+              <div className="contactLogo">📨</div>
+              <div className="headContact">Contact Us</div>
+              <div className="footContact">
+                Just want to say hi? We'd love to hear from you. We love our
+                customers and community!{" "}
+              </div>
+              <div className="btnWrapperContact">
+                <button className="btnContact">Contact Support</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
